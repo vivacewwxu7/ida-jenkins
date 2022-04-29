635fdba4659c5e968d92ed9e0578e1995cc9a4df
wencai <wencai@cn.ibm.com>
2022-04-29 10:27:07 +0800

    fix url

--- pipeline_by_name.sh
+++ pipeline_by_name.sh
@@ -13,8 +13,7 @@ fi;
 if [ "$INTERVAL" = "" ] ; then
 	export INTERVAL=10
 fi;
-echo "${IDA_HOST}/rest/v1/pipeline/build?pipelineName=${PIPELINE_NAME}"
-BUILD_RESULT=$(curl -X POST "${IDA_HOST}/rest/v1/pipeline/build?pipelineName=${PIPELINE_NAME}" -k --data-urlencode "userToken=${USER_TOKEN}")
+BUILD_RESULT=$(curl -X POST "${IDA_HOST}/rest/v1/pipeline/build/name?pipelineName=${PIPELINE_NAME}" -k --data-urlencode "userToken=${USER_TOKEN}")
 echo $BUILD_RESULT
 BUILD_ID="$(cut -d',' -f2 <<<"$BUILD_RESULT")"
 BUILD_ID="$(cut -d':' -f2 <<<"$BUILD_ID")"
